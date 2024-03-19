5c9349da46ac6d9fe8f86b47e8c1fcce27320f61
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-03-19 23:37:04 +0900

    feat: init home ai section

---
+++ components/sections/home/HomeAiSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="flex flex-col sm:flex-row gap-2">
+    <router-link
+      v-for="(data, idx) in AiList"
+      :key="idx"
+      :to="{
+        path: data.link
+      }"
+      class="px-4 py-2 border border-grey-700 rounded-md"
+    >
+      <h1>{{ data.name }}</h1>
+    </router-link>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const AiList = ref([
+  {
+    name: 'OpenAI',
+    link: '/openai'
+  },
+  {
+    name: 'Gemini',
+    link: '/gemini'
+  }
+])
+</script>
