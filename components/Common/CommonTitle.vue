74249bd925a02d5e2e84526d4810b3b7406d0f81
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-03-20 14:48:54 +0900

    feat: add common title component

---
+++ components/Common/CommonTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="my-16 text-5xl text-center select-none title">
+    {{ title }}
+  </h1>
+</template>
+
+<script lang="ts" setup>
+import type { CommonTitleProps } from '@/types/Common/CommonTitle'
+
+defineProps<CommonTitleProps>()
+</script>
+
+<style>
+.title {
+  background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
