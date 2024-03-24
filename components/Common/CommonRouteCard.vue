c9499016bec954e69e144c898e27f79ce3299c8b
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-03-24 13:03:32 +0900

    feat: update common title and add common route card

---
+++ components/Common/CommonRouteCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="flex flex-col sm:flex-row gap-4">
+    <NuxtLink
+      v-for="(card, idx) in cardList"
+      :key="idx"
+      :to="{
+        path: card.link
+      }"
+      class="group border-2 border-grey-900 px-8 py-4 hover:border-[#bd34fe] shadow-sm hover:shadow-md rounded-md"
+    >
+      <h1 class="group-hover:hover:text-[#bd34fe] text-gray-900 dark:text-white text-xl">
+        {{ card.name }}
+      </h1>
+    </NuxtLink>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import type { ICommonRouteCard } from '@/types/Common/CommonRouteCard'
+
+defineProps<{
+  cardList: ICommonRouteCard[],
+}>()
+</script>
--- components/Common/CommonTitle.vue
+++ components/Common/CommonTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="my-16 text-5xl text-center select-none title">
+  <h1 class="my-4 text-5xl text-center title">
     {{ title }}
   </h1>
 </template>
@@ -10,7 +10,7 @@ import type { CommonTitleProps } from '@/types/Common/CommonTitle'
 defineProps<CommonTitleProps>()
 </script>
 
-<style>
+<style scoped>
 .title {
   background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
   -webkit-background-clip: text;
